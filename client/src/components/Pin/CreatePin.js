de382b06c6558338074dcb131c5747a5f5621a00
Leo <coder.leo.le@gmail.com>
2019-09-24 11:18:52 +0700

    Upload images with Cloudinary

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from "react";
+import axios from 'axios'
 import { withStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
@@ -16,9 +17,23 @@ const CreatePin = ({ classes }) => {
   const [image, setImage] = useState("")
   const [content, setContent] = useState("")
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault()
-    console.log({ title, image, content })
+    const url = await handleImageUpload()
+    console.log({ url })
+  }
+
+  const handleImageUpload = async () => {
+    const data = new FormData()
+    data.append("file", image)
+    data.append("upload_preset", "geopins")
+    data.append("cloud_name", "dgu4zdjuf")
+
+    const response = await axios.post(
+      "https://api.cloudinary.com/v1_1/dgu4zdjuf/image/upload",
+      data,
+    )
+    return response.data.url
   }
 
   const handleDeleteDraft = () => {
