fb64ec52cf07dc3432636d9dd6014c6369b726be
Leo <coder.leo.le@gmail.com>
2019-09-27 11:39:26 +0700

    Deleting User Pins

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -10,6 +10,7 @@ import Context from '../context'
 import Blog from './Blog'
 import { useClient } from '../client'
 import { GET_PINS_QUERY } from '../graphql/queries'
+import { DELETE_PIN_MUTATION } from '../graphql/mutations'
 
 const INITIAL_VIEWPORT = {
   latitude: 37.7577,
@@ -75,6 +76,13 @@ const Map = ({ classes }) => {
   }
 
   const isAuthUser = () => state.currentUser._id === popup.author._id
+
+  const handleDeletePin = async (pin) => {
+    const variables = { pinId: pin._id }
+    const { deletePin } = await client.request(DELETE_PIN_MUTATION, variables)
+    dispatch({ type: "DELETE_PIN", payload: deletePin })
+    setPopup(null)
+  }
 
   return (
     <div className={classes.root}>
@@ -146,7 +154,7 @@ const Map = ({ classes }) => {
                 {popup.latitude.toFixed(6)}, {popup.longitude.toFixed(6)}
               </Typography>
               {isAuthUser() && (
-                <Button>
+                <Button onClick={() => handleDeletePin(popup)}>
                   <DeleteIcon className={classes.deleteIcon} />
                 </Button>
               )}
