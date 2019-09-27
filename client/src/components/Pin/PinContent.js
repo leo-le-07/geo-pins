47a9f9a1c57a183158bea1bbbe2ab8185f492d65
Leo <coder.leo.le@gmail.com>
2019-09-27 14:06:12 +0700

    Building out Components to Create/Display User Comments

--- client/src/components/Pin/PinContent.js
+++ client/src/components/Pin/PinContent.js
@@ -5,6 +5,8 @@ import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import FaceIcon from "@material-ui/icons/Face";
 import format from 'date-fns/format'
 
+import CreateComment from '../Comment/CreateComment'
+import Comments from '../Comment/Comments'
 import Context from '../../context'
 
 const PinContent = ({ classes }) => {
@@ -40,6 +42,8 @@ const PinContent = ({ classes }) => {
       >
         {content}
       </Typography>
+      <CreateComment />
+      <Comments comments={comments} />
     </div>
   )
 };
