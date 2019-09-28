d4c4d3ef14b28fe1060b14f281a35646ddd705a7
Leo <coder.leo.le@gmail.com>
2019-09-28 09:47:16 +0700

    Subscribing to Live Data Changes with Apollo Client

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -34,8 +34,7 @@ const CreatePin = ({ classes }) => {
         latitude,
         longitude,
       }
-      const { createPin } = await client.request(CREATE_PIN_MUTATION, variables)
-      dispatch({ type: "CREATE_PIN", payload: createPin })
+      await client.request(CREATE_PIN_MUTATION, variables)
       handleDeleteDraft()
     } catch (err) {
       console.error("Error create pin", err)
